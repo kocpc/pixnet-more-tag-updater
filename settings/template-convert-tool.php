6a696c4d631ad4ffc9c5b1b9b9e55684686de353
Hiram Huang <chown9835@gmail.com>
2016-06-01 06:49:09 +0000

    Add notice to start convert page.

--- settings/template-convert-tool.php
+++ settings/template-convert-tool.php
@@ -20,15 +20,20 @@ if( ! function_exists ('add_action') ) {
     <?php // Check success message ?>
     <?php if( isset( $_GET['success'] ) && $_GET['success'] === 'true' ): ?>
         <div class="notice notice-success is-dismissiable">
-            <p>轉換成功！</p>
+            <p><?php _e( '轉換成功！', PMTU_TEXT_DOMAIN ) ?></p>
         </div>
     <?php elseif( isset( $_GET['success'] ) && $_GET['success'] === 'false' ): ?>
         <div class="notice notice-error is-dismissiable">
-            <p>轉換失敗！</p>
+            <p><?php _e( '轉換失敗！', PMTU_TEXT_DOMAIN ) ?></p>
+        </div>
+    <?php else: ?>
+        <div class="notice notice-warning is-dismissiable">
+            <p><?php _e( '開始轉換之前，我們強烈建議備份您的資料庫。', PMTU_TEXT_DOMAIN ) ?></p>
         </div>
     <?php endif; ?>
     <?php // Check success message end ?>
     <p><?php _e( '一鍵將痞客邦 More 標籤轉換為 WordPress 格式。', PMTU_TEXT_DOMAIN ) ?></p>
+    <p><?php printf( __( '這將會把所有 %s 轉換成 %s 。', PMTU_TEXT_DOMAIN ), '<code>&lt;!-- more --&gt;</code>', '<code>&lt;!--more--&gt;</code>' ) ?></p>
     <form role="form" action="options.php" method="post">
         <?php wp_nonce_field( 'PMTU_START_CONVERT' ) ?>
         <input type="hidden" name="PMTU_START_CONVERT" value="1">
