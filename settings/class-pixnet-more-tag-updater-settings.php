e8bb2ba75c62ea062cedef768d8786eb50fe86ad
Hiram Huang <chown9835@gmail.com>
2016-06-01 04:43:18 +0000

    Create and initial settings class.

---
+++ settings/class-pixnet-more-tag-updater-settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * PIXNET More Tag Updater
+ *
+ * @author Hiram Huang <me@hiram.tw>
+ * @since 0.1
+ */
+
+class PIXNET_MORE_TAG_UPDATER_SETTINGS {
+    
+    const PMTU_SETTINGS_SLUG = 'pixnet-more-tag-updater-settings';
+    
+    /**
+     * Initiator.
+     * 
+     * @since 0.1
+     */
+    public static function init() {
+        
+    }
+    
+}
