0fad0af6d8c36e4ba69316f772627b4306ad12c1
Hiram Huang <chown9835@gmail.com>
2016-06-01 04:26:22 +0000

    Oh my OCD!

--- pixnet-more-tag-updater.php
+++ pixnet-more-tag-updater.php
@@ -22,7 +22,7 @@
 /**
  * If file is opened directly, return 403 error
  */
-if( ! function_exists ('add_action') ) {
+if( ! function_exists ( 'add_action' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
