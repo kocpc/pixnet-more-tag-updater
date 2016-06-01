6c240f6d5bde9736c57b941d0fb10f85e3bb84f6
Hiram Huang <chown9835@gmail.com>
2016-06-01 05:08:20 +0000

    Add hook to admin_menu and build a submenu under tools.

--- settings/class-pixnet-more-tag-updater-settings.php
+++ settings/class-pixnet-more-tag-updater-settings.php
@@ -17,6 +17,38 @@ class PIXNET_MORE_TAG_UPDATER_SETTINGS {
      */
     public static function init() {
         
+        // Hook to admin_menu to build a submenu
+        add_action( 'admin_menu', array( 'PIXNET_MORE_TAG_UPDATER_SETTINGS', 'build_submenu_under_tools' ) );
+        
+    }
+    
+    /**
+     * Build submenu under tools
+     * 
+     * @since 0.1
+     */
+    public static function build_submenu_under_tools() {
+        
+        add_submenu_page(
+            'tools.php',
+            __( '痞客邦 More 標籤轉換工具', PMTU_TEXT_DOMAIN ),
+            __( '痞客邦 More 轉換', PMTU_TEXT_DOMAIN ),
+            'manage_options',
+            self::PMTU_SETTINGS_SLUG,
+            array( 'PIXNET_MORE_TAG_UPDATER_SETTINGS', 'render_the_submenu_template' )
+        );
+        
+    }
+    
+    /**
+     * Submenu template render
+     * 
+     * @since 0.1
+     */
+    public static function render_the_submenu_template() {
+        
+        include_once( PMTU_BASE_FULL . '/settings/template-convert-tool.php' );
+        
     }
     
 }
